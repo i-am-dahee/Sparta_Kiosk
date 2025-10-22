771c11821618a28c31a439eecb0e94895c00f159
i-am-dahee <hxxly02@gmail.com>
2025-10-22 14:51:08 +0900

    [refactor] 텍스트 블록으로 변경 #1

--- src/kiosk01/Main.java
+++ src/kiosk01/Main.java
@@ -7,33 +7,36 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         while (true) {
-            System.out.println("[ SHAKESHACK MENU ]\n" +
-                    "1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거\n" +
-                    "2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거\n" +
-                    "3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거\n" +
-                    "4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거\n" +
-                    "0. 종료           | 종료");
+            System.out.println("""
+                [ SHAKESHACK MENU ]
+                1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거
+                2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거
+                3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거
+                4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거
+                0. 종료           | 종료
+                """);
+
 
             int choice = sc.nextInt();
 
             switch (choice) {
                 case 1:
-                    System.out.println("\n1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거\n");
+                    System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거\n");
                     break;
                 case 2:
-                    System.out.println("\n2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거\n");
+                    System.out.println("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거\n");
                     break;
                 case 3:
-                    System.out.println("\n3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거\n");
+                    System.out.println("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거\n");
                     break;
                 case 4:
-                    System.out.println("\n4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거\n");
+                    System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거\n");
                     break;
                 case 0:
                     System.out.println("\n프로그램을 종료합니다.");
                     System.exit(0);
                 default:
-                    System.out.println("\n다시 입력해 주세요.\n");
+                    System.out.println("다시 입력해 주세요.\n");
             }
         }
     }
