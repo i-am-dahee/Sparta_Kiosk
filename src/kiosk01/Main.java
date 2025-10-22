29539f0f96db26543f9fba88488071ea02891e14
i-am-dahee <hxxly02@gmail.com>
2025-10-22 16:14:44 +0900

    [feat] 순서 제어 클래스 분리 #1

--- src/kiosk01/Main.java
+++ src/kiosk01/Main.java
@@ -2,11 +2,9 @@ package kiosk01;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         List<MenuItem> menuItems = new ArrayList<>();
 
         // MenuItem 추가
@@ -15,28 +13,7 @@ public class Main {
         menuItems.add(new MenuItem("Cheeseburger", "W 6.9", "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
         menuItems.add(new MenuItem("Hamburger", "W 5.4", "비프패티를 기반으로 야채가 들어간 기본버거"));
 
-        while (true) {
-
-            // MenuItem 출력
-            System.out.println("[ SHAKESHACK MENU ]");
-            for (int i = 0; i < menuItems.size(); i++) {
-                System.out.printf("%d. %s%n", (i + 1), menuItems.get(i));
-            }
-            System.out.println("0. 종료             | 종료");
-
-            // 숫자 입력
-            int choice = sc.nextInt();
-
-            // 입력 받은 숫자에 따른 처리
-            if (choice == 0) {
-                System.out.println("\n프로그램을 종료합니다.");
-                System.exit(0);
-            } else if (choice >= 1 && choice <= menuItems.size()) {
-                MenuItem item = menuItems.get(choice - 1);
-                System.out.printf("%d. %s%n%n", choice, item);
-            } else {
-                System.out.println("다시 입력해 주세요.\n");
-            }
-        }
+        Kiosk kiosk = new Kiosk(menuItems);
+        kiosk.start();
     }
 }
